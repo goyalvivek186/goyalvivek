3b3306e2de2ba74adcaf58e11af826e35912359a
Vivek Goyal <goyalvivek186@gmail.com>
2022-05-12 02:15:40 +0530

    portrait

---
+++ Files/script.js
@@ -0,0 +1,6 @@
+screen.orientation.addEventListener("change", function () {
+  if (screen.orientation.type == "portrait-primary") {
+    alert("For better experience, change the screen orientation to Landscape");
+  }
+  console.log("Current orientation is " + screen.orientation.type);
+});
